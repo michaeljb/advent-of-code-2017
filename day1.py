f270702a7718aa19d61d03d5f6162c85bc3b8dd9
Michael Brandt <mbrandt@colorado.edu>
2017-12-18 12:22:09 -0700

    day 1, parts 1 and 2

--- day1.py
+++ day1.py
@@ -4,28 +4,44 @@ import click
 
 
 @click.command()
-@click.argument('part', type=int, default=1)
 @click.argument('input_file', type=click.Path(exists=True))
-def main(part, input_file):
-    return {
-        1: part1
-    }[part](input_file)
+def cli(input_file):
+    the_input = parse_input(input_file)
+
+    print(part1(the_input))
+    print(part2(the_input))
 
 
-def part1(input_file):
+def parse_input(input_file):
     input_string = open(input_file, 'r').read().strip()
-    digits = [int(num) for num in list(input_string)]
+    return [int(num) for num in list(input_string)]
 
-    digits.append(digits[0])
 
+def part1(digits):
+    def next_index_func(index):
+        return (index + 1) % len(digits)
+
+    return sum_when_next_matches(digits, next_index_func)
+
+
+def part2(digits):
+    def next_index_func(index):
+        return (index + int(len(digits) / 2)) % len(digits)
+
+    return sum_when_next_matches(digits, next_index_func)
+
+
+def sum_when_next_matches(digits, next_index_func):
     the_sum = 0
 
-    for i in range(len(digits) - 1):
-        if digits[i] == digits[i+1]:
-            the_sum += digits[i]
+    for index in range(len(digits)):
+        next_index = next_index_func(index)
 
-    print(the_sum)
+        if digits[index] == digits[next_index]:
+            the_sum += digits[index]
+
+    return the_sum
 
 
 if __name__ == '__main__':
-    main()
+    cli()
